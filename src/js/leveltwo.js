9684f90e4ddbb3ffde00599d71419cc4aa35d158
Lucas <sennavdkoppel@gmail.com>
2024-06-24 15:47:20 +0200

    dialogue level 2

--- src/js/leveltwo.js
+++ src/js/leveltwo.js
@@ -8,6 +8,16 @@ import { InvisWallAdder } from "./inviswalladder.js"
 import { SceneTransition } from "./sceneTransition.js"
 
 export class LevelTwo extends Scene {
+
+    dialogueText = [
+        'Humans have existed for millenia, this time has made them \nbecome ambitious. However, they use it in an inefficient way. They \nwant to see the future, and be able to control it.', 
+        'The only way they know how is by creating worlds of fiction \nand dreams. We alchemists don’t have that limitation. We don’t have \nto limit our ambitions with futile fantasies. We can create any world \nwe desire.', 
+    ]
+    otherDialogue = [
+        'Humans have existed for millenia. All this time has made them \nbecome ambitious. They want to see the future, they want to \ncontrol it. But the only way they can is by creating fictional worlds.', 
+        'We alchemists don’t have that privilege. We have the power to \ncreate what we want, but this also means we have to take the \nresponsibility that comes with it. Selflessness is the key in the \nwork that we do.', 
+    ]
+
     onInitialize(engine) {
 
     }
@@ -66,6 +76,14 @@ export class LevelTwo extends Scene {
     }
     changeScene() {
         this.engine.goToScene('levelthree')
+    }
+    sceneDialogue(number) {
+        if (this.actors[8].pos.x > 1890) {
+            this.actors[10].dialogueFlow(this.dialogueText[number])
+        } else {
+            this.actors[10].dialogueFlow(this.otherDialogue[number])
+        }
+        
     }
     addInvisWall() {
         if (this.engine.wallSpawned == true) {
